2501e894bac3378835373785c14af1494597bbad
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-07-21 08:41:13 +0530

    Update func_ptr.c

--- pointers/func_ptr.c
+++ pointers/func_ptr.c
@@ -8,3 +8,38 @@ int main() {
     (*funcs[1])();
     return 0;
 }
+
+=====================================
+    
+#include <stdio.h>
+
+void execute_op(int x, int y, int (*operation)(int, int)) {
+    printf("Operation Result: %d\n", operation(x, y));
+}
+
+int multiply(int a, int b) { return a * b; }
+
+int main() {
+    // Passing the multiply function as an argument
+    execute_op(4, 5, multiply);
+    return 0;
+}
+
+====================================
+    #include <stdio.h>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int (*fp)(int, int);   // Function pointer
+
+    fp = add;              // Store function address
+
+    printf("%d\n", fp(10, 20));
+
+    return 0;
+}
