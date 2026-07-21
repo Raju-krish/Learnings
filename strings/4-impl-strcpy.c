3e96d3959f65fdd10762fceb9ce83d25905bf78d
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-07-21 08:58:28 +0530

    Update 4-impl-strcpy.c

--- strings/4-impl-strcpy.c
+++ strings/4-impl-strcpy.c
@@ -20,3 +20,32 @@ int main(int argc, char *argv[])
     my_strcpy(new_arr, arr);
     printf("new_arr: %s\n", new_arr);
 }
+
+===============================================
+    #include <stdio.h>
+
+void my_strcpy(char dest[], char src[])
+{
+    int i = 0;
+
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+
+    dest[i] = '\0';   // Copy the null character
+}
+
+int main()
+{
+    char src[] = "Hello";
+    char dest[20];
+
+    my_strcpy(dest, src);
+
+    printf("Source      : %s\n", src);
+    printf("Destination : %s\n", dest);
+
+    return 0;
+}
