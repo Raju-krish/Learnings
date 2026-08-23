cf93f3d5063c3c6a6c8bb34ccdcd932cf69cd93d
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-08-23 23:03:12 +0530

    Update 1-process.c

--- os-concepts/1-process.c
+++ os-concepts/1-process.c
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     if(fork() == 0) {
+        # getsid - Session ID 
         printf("Hey I'm the child: %d | %d\n", getpid(), getsid(getpid()));
     } else {
         printf("Hey I'm the parent: %d | %d\n", getpid(), getsid(getpid()));
