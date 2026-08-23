578e328f44846a80329d96c1d2dbedb797ef672d
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-08-23 22:44:16 +0530

    Update next_power_of_two.c

--- math/next_power_of_two.c
+++ math/next_power_of_two.c
@@ -12,5 +12,20 @@ int main(int argc, char *argv[])
 {
     int val = 0;
     scanf("%d", &val);
-    printf("%d is the next power of two\n", next_power(val));
+    printf("%d is the next power of two from the given Value\n", next_power(val));
 }
+
+==========================================================================
+power = 1
+val = 10
+
+| Iteration | `power` | `power < 10`? | After `power *= 2` |
+| --------: | ------: | :-----------: | -----------------: |
+|         1 |       1 |      Yes      |                  2 |
+|         2 |       2 |      Yes      |                  4 |
+|         3 |       4 |      Yes      |                  8 |
+|         4 |       8 |      Yes      |                 16 |
+|         5 |      16 |       No      |               Stop |
+
+Time complexity = O(log n)
+Space complexity = O(1)
